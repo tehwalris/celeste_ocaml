9feaaaabff7eb60ed361bcb81aa8659b727e25b2
Philippe Voinov <philippevoinov@gmail.com>
2022-01-09 23:12:49 +0100

    Notes about flr and the objects array

--- celeste_ocaml.ml
+++ celeste_ocaml.ml
@@ -18,3 +18,47 @@ let range_y = Apron.Abstract1.bound_variable man abs var_y in
 let range_target = Apron.Abstract1.bound_variable man abs var_target in
 Format.printf "var_y = %a@.var_target = %a@." Apron.Interval.print range_y
   Apron.Interval.print range_target
+
+(*
+rem0 = ...
+arg0 = rem0 + 0.5
+amount0 = flr(arg0)
+rem1 = rem0 - amount0
+
+flr([12.2 15.6]) = [12 15]
+flr([12.2 13[) = [12 12]
+flr([13 14[) = [13 13]
+flr([14 15[) = [14 14]
+flr([15 15.6[) = [15 15]
+
+a - flr(a) = b
+flr(a) = a - b
+
+flr(a) <= a
+b >= 0 && b < 1
+
+a - 1 < flr(a) <= a
+*)
+
+(*
+
+player
+player_spawn
+spring
+balloon
+fall_floor
+smoke - dead
+fruit
+fly_fruit
+lifeup - dead
+fake_wall
+key
+chest
+platform
+message - dead
+big_chest
+orb
+flag - dead
+room_title - dead
+
+*)
