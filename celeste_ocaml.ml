286e55a5378296857850f7b651996620e0779e17
Philippe Voinov <philippevoinov@gmail.com>
2022-01-09 21:06:49 +0100

    Make example similar to the case in celeste-ts

--- celeste_ocaml.ml
+++ celeste_ocaml.ml
@@ -1,21 +1,20 @@
 let man = Oct.manager_alloc () in
 
-let var_x = Apron.Var.of_string "x" in
 let var_y = Apron.Var.of_string "y" in
+let var_target = Apron.Var.of_string "target" in
 
-let env = Apron.Environment.make [| var_x; var_y |] [||] in
+let env = Apron.Environment.make [| var_y; var_target |] [||] in
 Format.printf "env = %a@." (fun x -> Apron.Environment.print x) env;
 
-let tab = Apron.Parser.lincons1_of_lstring env [ "x>=5"; "x<10" ] in
+let tab =
+  Apron.Parser.lincons1_of_lstring env
+    [ "target>=0"; "target<=32"; "y>=40"; "y<=128"; "y<target+16" ]
+in
 Format.printf "tab = %a@." (fun x -> Apron.Lincons1.array_print x) tab;
 let abs = Apron.Abstract1.of_lincons_array man env tab in
 Format.printf "abs = %a@." Apron.Abstract1.print abs;
 
-let tab = Apron.Parser.lincons1_of_lstring env [ "x>=5"; "x<10"; "y < x" ] in
-Format.printf "tab = %a@\n" (fun x -> Apron.Lincons1.array_print x) tab;
-let abs = Apron.Abstract1.of_lincons_array man env tab in
-Format.printf "abs = %a@." Apron.Abstract1.print abs;
-
-let x = Apron.Abstract1.bound_variable man abs var_x in
-let y = Apron.Abstract1.bound_variable man abs var_y in
-Format.printf "x = %a@.y = %a@." Apron.Interval.print x Apron.Interval.print y
+let range_y = Apron.Abstract1.bound_variable man abs var_y in
+let range_target = Apron.Abstract1.bound_variable man abs var_target in
+Format.printf "var_y = %a@.var_target = %a@." Apron.Interval.print range_y
+  Apron.Interval.print range_target
