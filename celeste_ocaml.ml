8d9c3184e1b342c978b27258357fe9d54bf9571d
Philippe Voinov <philippevoinov@gmail.com>
2022-01-09 20:03:11 +0100

    Print intervals

--- celeste_ocaml.ml
+++ celeste_ocaml.ml
@@ -4,14 +4,18 @@ let var_x = Apron.Var.of_string "x" in
 let var_y = Apron.Var.of_string "y" in
 
 let env = Apron.Environment.make [| var_x; var_y |] [||] in
-Format.printf "env = %a@\n" (fun x -> Apron.Environment.print x) env;
+Format.printf "env = %a@." (fun x -> Apron.Environment.print x) env;
 
 let tab = Apron.Parser.lincons1_of_lstring env [ "x>=5"; "x<10" ] in
-Format.printf "tab = %a@\n" (fun x -> Apron.Lincons1.array_print x) tab;
+Format.printf "tab = %a@." (fun x -> Apron.Lincons1.array_print x) tab;
 let abs = Apron.Abstract1.of_lincons_array man env tab in
 Format.printf "abs = %a@." Apron.Abstract1.print abs;
 
 let tab = Apron.Parser.lincons1_of_lstring env [ "x>=5"; "x<10"; "y < x" ] in
 Format.printf "tab = %a@\n" (fun x -> Apron.Lincons1.array_print x) tab;
 let abs = Apron.Abstract1.of_lincons_array man env tab in
-Format.printf "abs = %a@." Apron.Abstract1.print abs
+Format.printf "abs = %a@." Apron.Abstract1.print abs;
+
+let x = Apron.Abstract1.bound_variable man abs var_x in
+let y = Apron.Abstract1.bound_variable man abs var_y in
+Format.printf "x = %a@.y = %a@." Apron.Interval.print x Apron.Interval.print y
