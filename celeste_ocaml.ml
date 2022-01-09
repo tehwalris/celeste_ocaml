97ce157667b77c320bc98db2c9ed92ea23ced70c
Philippe Voinov <philippevoinov@gmail.com>
2022-01-09 19:45:02 +0100

    Get apron to work

--- celeste_ocaml.ml
+++ celeste_ocaml.ml
@@ -1,3 +1,17 @@
-type service_info = { y : int }
+let man = Oct.manager_alloc () in
 
-(* let () = print_endline "Hello, World!" *)
+let var_x = Apron.Var.of_string "x" in
+let var_y = Apron.Var.of_string "y" in
+
+let env = Apron.Environment.make [| var_x; var_y |] [||] in
+Format.printf "env = %a@\n" (fun x -> Apron.Environment.print x) env;
+
+let tab = Apron.Parser.lincons1_of_lstring env [ "x>=5"; "x<10" ] in
+Format.printf "tab = %a@\n" (fun x -> Apron.Lincons1.array_print x) tab;
+let abs = Apron.Abstract1.of_lincons_array man env tab in
+Format.printf "abs = %a@." Apron.Abstract1.print abs;
+
+let tab = Apron.Parser.lincons1_of_lstring env [ "x>=5"; "x<10"; "y < x" ] in
+Format.printf "tab = %a@\n" (fun x -> Apron.Lincons1.array_print x) tab;
+let abs = Apron.Abstract1.of_lincons_array man env tab in
+Format.printf "abs = %a@." Apron.Abstract1.print abs
