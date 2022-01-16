8b4e2e26205b760f978c10c23a682cc6e75eba33
Philippe Voinov <philippevoinov@gmail.com>
2022-01-16 22:18:39 +0100

    WIP: Create empty tables

--- celeste_ocaml.ml
+++ celeste_ocaml.ml
@@ -20,6 +20,7 @@ type any_value = Concrete of concrete_value | Abstract of abstract_value
 [@@deriving show]
 
 type array_table = any_value list [@@deriving show]
+
 type object_table = any_value StringMap.t
 
 let pp_object_table (f : Format.formatter) (_t : object_table) =
@@ -31,11 +32,16 @@ let pp_object_table (f : Format.formatter) (_t : object_table) =
           (fun (k, v) -> k ^ " = " ^ show_any_value v)
           (StringMap.to_seq _t)))
 
-type state = { global : object_table } [@@deriving show]
+type heap_value = ArrayTable of array_table | ObjectTable of object_table
+[@@deriving show]
+
+type state = { global : object_table; heap : heap_value list } [@@deriving show]
+
 type identifier = Identifier of string [@@deriving show]
 
 type expression =
   | ExpressionNumber of pico_number
+  | ExpressionBoolean of bool
   | ExpressionTable of (identifier * expression) list
 [@@deriving show]
 
@@ -44,15 +50,33 @@ type statement = StatmentAssignment of identifier * expression
 
 type program = statement list [@@deriving show]
 
+let set_global_variable (name : string) (value : any_value) (state : state) =
+  { state with global = StringMap.add name value state.global }
+
 let interpret_statement (stmt : statement) (state : state) : state =
   match stmt with
-  | StatmentAssignment (Identifier name, expr) ->
-      let v =
-        match expr with
-        | ExpressionNumber n -> Concrete (ConcreteNumber n)
-        | ExpressionTable _ -> raise (Failure "not implemented")
-      in
-      { global = StringMap.add name v state.global }
+  | StatmentAssignment (Identifier name, expr) -> (
+      match expr with
+      | ExpressionNumber n ->
+          {
+            state with
+            global =
+              StringMap.add name (Concrete (ConcreteNumber n)) state.global;
+          }
+      | ExpressionBoolean b ->
+          {
+            state with
+            global =
+              StringMap.add name (Concrete (ConcreteBoolean b)) state.global;
+          }
+      | ExpressionTable [] ->
+          let i = List.length state.heap in
+          {
+            global =
+              StringMap.add name (Concrete (ConcreteReference i)) state.global;
+            heap = state.heap @ [ ObjectTable StringMap.empty ];
+          }
+      | ExpressionTable _ -> raise (Failure "not implemented"))
 
 let debug_program (state : state) (program : program) =
   let state =
@@ -71,6 +95,9 @@ let example_program : program =
     StatmentAssignment
       (Identifier "b", ExpressionNumber (pico_number_of_int 456));
     StatmentAssignment (Identifier "a", ExpressionNumber (pico_number_of_int 0));
+    StatmentAssignment (Identifier "b", ExpressionBoolean true);
+    StatmentAssignment (Identifier "c", ExpressionTable []);
+    StatmentAssignment (Identifier "d", ExpressionTable []);
   ]
 
-let () = debug_program { global = StringMap.empty } example_program
+let () = debug_program { global = StringMap.empty; heap = [] } example_program
