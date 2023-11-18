d3361b0224482938c45d8f88b682dfb92ac861c0
Philippe Voinov <philippevoinov@gmail.com>
2023-11-18 21:37:40 +0100

    Assert what the Lua program outputs

--- lua_tests.ml
+++ lua_tests.ml
@@ -13,16 +13,19 @@ let check_lua_status state status =
       in
       failwith err_msg
 
-let handle_print_from_lua state =
-  if Lua.gettop state <> 1 then failwith "Wrong number of arguments";
-  let s = Option.get @@ Lua.tostring state 1 in
-  Printf.printf "Lua wants to print: %s\n" s;
-  0
-
 let%test_unit _ =
+  let collected_prints = ref [] in
+  let handle_print_from_lua state =
+    if Lua.gettop state <> 1 then failwith "Wrong number of arguments";
+    let s = Option.get @@ Lua.tostring state 1 in
+    collected_prints := s :: !collected_prints;
+    0
+  in
+
   let state = LuaL.newstate () in
   LuaL.openlibs state;
   Lua.pushcfunction state handle_print_from_lua;
   Lua.setglobal state "print";
   check_lua_status state @@ LuaL.loadfile state "hello_world.lua";
-  check_lua_status state @@ Lua.pcall state 0 0 0
+  check_lua_status state @@ Lua.pcall state 0 0 0;
+  assert (List.rev !collected_prints = [ "walrus" ])
