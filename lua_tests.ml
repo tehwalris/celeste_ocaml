388a0e5eeec271ba3748a9429af22e76c363ff21
Philippe Voinov <philippevoinov@gmail.com>
2023-11-18 21:32:07 +0100

    Print using native OCaml function

--- lua_tests.ml
+++ lua_tests.ml
@@ -13,8 +13,16 @@ let check_lua_status state status =
       in
       failwith err_msg
 
+let handle_print_from_lua state =
+  if Lua.gettop state <> 1 then failwith "Wrong number of arguments";
+  let s = Option.get @@ Lua.tostring state 1 in
+  Printf.printf "Lua wants to print: %s\n" s;
+  0
+
 let%test_unit _ =
   let state = LuaL.newstate () in
   LuaL.openlibs state;
+  Lua.pushcfunction state handle_print_from_lua;
+  Lua.setglobal state "print";
   check_lua_status state @@ LuaL.loadfile state "hello_world.lua";
   check_lua_status state @@ Lua.pcall state 0 0 0
