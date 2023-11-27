7382ac2414733a4498d5cd98b1cd16978e0e7fac
Philippe Voinov <philippevoinov@gmail.com>
2023-11-27 22:01:57 +0100

    lua_tests/properties.lua

--- lua_tests.ml
+++ lua_tests.ml
@@ -77,3 +77,4 @@ let test_lua filename =
 let%test_unit _ = test_lua "hello_world.lua"
 let%test_unit _ = test_lua "if_scopes.lua"
 let%test_unit _ = test_lua "every_kind_of_if_else.lua"
+let%test_unit _ = test_lua "properties.lua"
